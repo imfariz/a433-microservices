9de38c72efcbae23d9c82a060c26d4261308347f
imfariz <farizdhn@gmail.com>
2023-10-13 00:49:41 +0800

    Adding Tag in Build latest

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # Membangun Image sesuai Dockerfile
-docker build -t karsajobs .
+docker build -t karsajobs:latest .
 
 # Mengubah nama image sesuai format GitHub Packages
-docker tag karsajobs ghcr.io/imfariz/karsajobs
+docker tag karsajobs:latest ghcr.io/imfariz/karsajobs:latest
 
 # Login ke Github Container Registry
 echo $PASSWORD_GH | docker login ghcr.io -u imfariz --password-stdin
 
 # Mengunggah image ke GitHub Packages
-docker push ghcr.io/imfariz/karsajobs
+docker push ghcr.io/imfariz/karsajobs:latest
